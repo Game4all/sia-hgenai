0687658aac9fe48b3f470aa1a82819ddc370d94f
Rémy Chen <pmc2000@gmx.Fr>
2025-02-01 21:19:18 +0100

    scrapper add accident history(url and df)

--- app/test_scrapping.py
+++ app/test_scrapping.py
@@ -4,5 +4,6 @@ from utils.scrapper import scrapper
 if __name__ == '__main__':
     # st.title("Test")
     scrap = scrapper(3,"mistral.mistral-7b-instruct-v0:2","AIzaSyD0EjP3Z9YChWnGpnKhDAc1Ithm2Npdpdk","2711c54ca8c8e4c35")
-    doc = scrap.find_doc("Picardie",["SRADDET", "SDAGE"],True,True)
-    print([docs["url"] for docs in doc])
+    # doc = scrap.find_doc("Picardie",["SRADDET", "SDAGE"],True,True)
+    # print([docs["url"] for docs in doc])
+    print(scrap.get_accident_history("paris",True))
